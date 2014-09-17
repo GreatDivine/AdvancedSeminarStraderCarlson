290a25ac62b5dc992c052621d7108a22ac3b643a
Kyle Strader <kyle.strader@mymail.champlain.edu>
2014-09-17 12:34:18 -0400

    game settings file added

---
+++ ProjectT1_CodyCarlson_KyleStrader/src/util/GameSettings.java
@@ -0,0 +1,12 @@
+package util;
+
+public class GameSettings {
+
+	public static final double FRAME_RATE = 60; //frames per second
+	public static final double UPDATE_TIME = 1.0 / FRAME_RATE; //milliseconds
+	public static final double NANOSECONDS_TO_SECONDS = 1000000000.0; //1.0E9 nanoseconds per 1 second
+	public static final int TIME_BETWEEN_WAVE = 1; //seconds
+	public static final int FRAME_WIDTH = 512; //pixels
+	public static final int FRAME_HEIGHT = 512; //pixels
+
+}
