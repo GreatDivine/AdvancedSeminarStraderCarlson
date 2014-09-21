f1354369cbac7a9e2e224239dd344716c28e415e
Kyle Strader <kyle.strader@mymail.champlain.edu>
2014-09-21 15:50:24 -0400

    Added ProjectileManager and TowerManager

---
+++ ProjectT1_CodyCarlson_KyleStrader/src/util/ProjectileManager.java
@@ -0,0 +1,49 @@
+package util;
+
+import gameItems.Projectile;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectileManager {
+	
+	private List<Projectile> mProjectiles;
+	private double mTimePassed;
+	private double mCurTime;
+	private double mPrevTime;
+	private double mStartTime;
+	
+	public ProjectileManager(){
+		mProjectiles = new ArrayList<Projectile>();
+		mTimePassed = 0;
+		mStartTime = (double)System.nanoTime() / GameSettings.NANOSECONDS_TO_SECONDS;
+		mCurTime = mStartTime;
+		mPrevTime = 0;
+	}
+	
+	public void addProjectile(int xPos, int yPos, float xVel, float yVel){
+		mProjectiles.add(new Projectile(xPos, yPos, xVel, yVel));
+	}
+	
+	
+	public void update(long timeNS){
+		for(Projectile p:mProjectiles){
+			p.update(timeNS);
+		}
+	}
+	
+	public void paint(Graphics g){
+		for(Projectile p:mProjectiles){
+			p.paint(g);
+		}
+	}
+	
+	public Projectile getProjectile(int index){
+		return mProjectiles.get(index);
+	}
+	
+	public int getNumProjectiles(){
+		return mProjectiles.size();
+	}
+}
---
+++ ProjectT1_CodyCarlson_KyleStrader/src/util/TowerManager.java
@@ -0,0 +1,32 @@
+package util;
+
+import gameItems.Tower;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TowerManager {
+	
+	private List<Tower> mTowers;
+	
+	public TowerManager(){
+		mTowers = new ArrayList<Tower>();
+	}
+	
+	public void addTower(int x, int y, int w, int h, int fireRad, float shotDelay){
+		mTowers.add(new Tower(x, y, w, h, fireRad, shotDelay));
+	}
+	
+	public void update(long timeNS){
+		for(Tower t:mTowers){
+			t.update(timeNS);
+		}
+	}
+	
+	public void paint(Graphics g){
+		for(Tower t:mTowers){
+			t.paint(g);
+		}
+	}
+}
