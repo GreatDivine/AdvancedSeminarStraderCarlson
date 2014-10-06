e318fe7945fea7c9c6152d7aa7e8ca105987a7cf
Kyle Strader <kyle.strader@mymail.champlain.edu>
2014-10-05 22:54:37 -0400

    Revert "Added Player and PlayerUI"
    This reverts commit ab957f0644eb8ddb12797ef10d11333837e88025.
    
    Conflicts:
    	ProjectT2_CodyCarlson_KyleStrader/ProjectT2_CodyCarlson_KyleStrader/bin/gameItems/tower/Projectile.class
    	ProjectT2_CodyCarlson_KyleStrader/ProjectT2_CodyCarlson_KyleStrader/bin/gameItems/tower/ProjectileManager.class
    	ProjectT2_CodyCarlson_KyleStrader/ProjectT2_CodyCarlson_KyleStrader/bin/main/Room.class
    	ProjectT2_CodyCarlson_KyleStrader/ProjectT2_CodyCarlson_KyleStrader/bin/unitTests/ProjectileManagerTests.class
    	ProjectT2_CodyCarlson_KyleStrader/ProjectT2_CodyCarlson_KyleStrader/bin/util/GameSettings.class
    	ProjectT2_CodyCarlson_KyleStrader/ProjectT2_CodyCarlson_KyleStrader/bin/waves/Wave$mZombieType.class
    	ProjectT2_CodyCarlson_KyleStrader/ProjectT2_CodyCarlson_KyleStrader/bin/waves/Wave.class
    	ProjectT2_CodyCarlson_KyleStrader/ProjectT2_CodyCarlson_KyleStrader/bin/waves/WaveManager.class
    	ProjectT2_CodyCarlson_KyleStrader/bin/player/Observable.class
    	ProjectT2_CodyCarlson_KyleStrader/bin/player/Observer.class
    	ProjectT2_CodyCarlson_KyleStrader/bin/player/Player.class
    	ProjectT2_CodyCarlson_KyleStrader/src/gameItems/tower/ProjectileManager.java
    	ProjectT2_CodyCarlson_KyleStrader/src/main/Room.java
    	ProjectT2_CodyCarlson_KyleStrader/src/player/Observable.java
    	ProjectT2_CodyCarlson_KyleStrader/src/player/Observer.java
    	ProjectT2_CodyCarlson_KyleStrader/src/player/Player.java
    	ProjectT2_CodyCarlson_KyleStrader/src/player/PlayerUI.java

--- ProjectT2_CodyCarlson_KyleStrader/src/player/Observable.java
+++
@@ -1,7 +0,0 @@
-package player;
-
-public interface Observable {
-	
-	public void sendMessage();
-
-}
--- ProjectT2_CodyCarlson_KyleStrader/src/player/Observer.java
+++
@@ -1,6 +0,0 @@
-package player;
-
-public interface Observer {
-	
-	public void process(int hp, int cash);
-}
